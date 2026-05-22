3404632b4c10fd25c08fe2fd070b2e2e93acee53
htmlcss1234848 <resvyrj50@gmail.com>
2026-05-22 17:32:36 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'app/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  runApp(const IPTVPlayerApp());
+}
